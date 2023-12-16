7efadf70ed86a2987ec6632a9f43fdde9f75f2c2
hrblr <aidanroley@icloud.com>
2023-12-16 14:40:40 -0500

    Index Register, Stack, program counter declared

--- CPU.py
+++ CPU.py
@@ -2,9 +2,13 @@ import pygame
 
 class CPU:
 
-    def __init__(self): ## i will use an array as a stack instead of importing a stack 
+    def __init__(self,initial_pc=0x200): ## i will use an array as a stack instead of importing a stack 
         self.stack = [0] * 16
         self.stack_pointer = -1
+        self.pc = initial_pc
+        self.index_register = 0
+
+
 
     def push(self, value):
         self.stack_pointer += 1
